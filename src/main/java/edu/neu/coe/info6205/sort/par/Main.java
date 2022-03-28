4f4bb789d76072236fa2cceec0d37511b04269a9
AnushaPoojaryNEU <poojary.an@northeastern.edu>
2022-03-28 14:37:44 -0400

    Assignment 4

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -4,10 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 /**
@@ -17,35 +14,24 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
+
+        // default values in case command line arguments are not passed
+        configuration.put("c", 2000); // cutoff mark for system sort
+        configuration.put("s", 2000000); // size of the array
+
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
-        Random random = new Random();
-        int[] array = new int[2000000];
-        ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
-            }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
+        ArrayList<Long> timeList;
 
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
-        }
+        ParSort.cutoff = configuration.get("c");
+        if (configuration.containsKey("t")) timeList = computeSortTimeVaryingCutoff();
+        else timeList = computeSortTimeVaryingThreadCount();
         try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
+            FileOutputStream fis = new FileOutputStream("./src/results/parallelsort/co_" + ParSort.cutoff + "as_" + configuration.get("s") + "result.csv");
             OutputStreamWriter isr = new OutputStreamWriter(fis);
             BufferedWriter bw = new BufferedWriter(isr);
             int j = 0;
             for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
+                String content = (int) Math.pow(2, j) + "," + i + "\n";
                 j++;
                 bw.write(content);
                 bw.flush();
@@ -57,25 +43,72 @@ public class Main {
         }
     }
 
+    private static ArrayList<Long> computeSortTimeVaryingThreadCount() {
+        Random random = new Random();
+        int[] array = new int[configuration.get("s")];
+        ArrayList<Long> timeList = new ArrayList<>();
+        for (int k = 0; k < 11; k++) {
+            int threadCount = (int) Math.pow(2,k);
+            ParSort.forkJoinPool = new ForkJoinPool(threadCount);
+            long time;
+            long startTime = System.currentTimeMillis();
+            for (int t = 0; t < 10; t++) {
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                ParSort.sort(array, 0, array.length);
+            }
+            long endTime = System.currentTimeMillis();
+            time = (endTime - startTime);
+            timeList.add(time);
+
+            System.out.println("cutoff：" + ParSort.cutoff + "  :threads：" + threadCount + "  :arraySize：" + configuration.get("s") + "  :Time:  " + time + "ms");
+
+        }
+        return timeList;
+    }
+
+    private static ArrayList<Long> computeSortTimeVaryingCutoff() {
+        Random random = new Random();
+        int[] array = new int[configuration.get("s")];
+        ArrayList<Long> timeList = new ArrayList<>();
+        int threadCount = configuration.get("t");
+        ParSort.cutoff = configuration.get("s")/2 - 600000;
+        for (int k = 0; k < 10; k++) {
+            ParSort.cutoff = ParSort.cutoff + 100000;
+            ParSort.forkJoinPool = new ForkJoinPool(32);
+            long time;
+            long startTime = System.currentTimeMillis();
+            for (int t = 0; t < 10; t++) {
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                ParSort.sort(array, 0, array.length);
+            }
+            long endTime = System.currentTimeMillis();
+            time = (endTime - startTime);
+            timeList.add(time);
+
+            System.out.println("cutoff：" + ParSort.cutoff + "  :threads：" + threadCount + "  :arraySize：" + configuration.get("s") + "  :Time:  " + time + "ms");
+
+        }
+        return timeList;
+    }
+
     private static void processArgs(String[] args) {
         String[] xs = args;
         while (xs.length > 0)
             if (xs[0].startsWith("-")) xs = processArg(xs);
+        System.out.println(configuration);
     }
 
     private static String[] processArg(String[] xs) {
-        String[] result = new String[0];
+        String[] result = new String[xs.length-2];
         System.arraycopy(xs, 2, result, 0, xs.length - 2);
         processCommand(xs[0], xs[1]);
         return result;
     }
 
     private static void processCommand(String x, String y) {
-        if (x.equalsIgnoreCase("N")) setConfig(x, Integer.parseInt(y));
-        else
-            // TODO sort this out
-            if (x.equalsIgnoreCase("P")) //noinspection ResultOfMethodCallIgnored
-                ForkJoinPool.getCommonPoolParallelism();
+        if (x.equalsIgnoreCase("-s")) setConfig("s", Integer.parseInt(y));
+        else if (x.equalsIgnoreCase("-t")) setConfig("t", Integer.parseInt(y));
+        else if (x.equalsIgnoreCase("-c")) setConfig("c", Integer.parseInt(y));
     }
 
     private static void setConfig(String x, int i) {
